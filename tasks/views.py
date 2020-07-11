ea802af699876cb880d553ec192492d770d265c1
Roman-kazi <rpgk2016@gmail.com>
2020-07-11 18:18:57 +0530

    basic task adding and viewing app ready

--- tasks/views.py
+++ tasks/views.py
@@ -3,18 +3,17 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
     #priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
-
-
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks":tasks   # the name in "" is the name that the html will have acces too
+        "tasks":request.session["tasks"]   # the name in "" is the name that the html will have acces too
     })
 
 def add(request):
@@ -23,7 +22,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
